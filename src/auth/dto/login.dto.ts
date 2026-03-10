aa88590a6d36eb0eead503b85c00e5fcba829049
Sai-guru <lpnkaveri@gmail.com>
2026-03-10 22:12:38 +0530

    implementing auth-controller next

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -9,4 +9,5 @@ export class LoginDto {
     @MinLength(6, { message: "Password must be at least 6 characters long" })
     @MaxLength(15, { message: "Password must not be more than 15 characters long" })
     password!: string;
+    
 }
